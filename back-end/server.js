e62680242c50dbb9402a30ebbe32d36a3d4fcae2
Mauricio Arismendi <arismendimauricio@gmail.com>
2019-09-25 23:11:31 -0300

    register

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,10 @@
 var express = require('express')
 var cors = require('cors')
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
 var app = express()
 
+var User = require('./models/User')
 
 var posts = [
     { message: 'hola'},
@@ -10,8 +13,30 @@ var posts = [
 ]
 
 app.use(cors())
+app.use(bodyParser.json())
+
 app.get('/posts', (req, res) => {
     res.send(posts)
+})
+
+
+app.post('/register', (req, res) => {
+    var userData = req.body
+
+    var user = new User(userData)    
+    user.save((err, result) => {
+        if (err)
+            console.log('There is an error saving the user')
+           
+        res.sendStatus(200)    
+    })
+    
+})
+
+
+mongoose.connect('mongodb+srv://mauridev:ElectromecanicA21@maurimongo-pbqll.mongodb.net/pssocial?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if(!err)
+        console.log('Connected')
 })
 
 app.listen(3001)
