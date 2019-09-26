4368822731c80da1132c1fc50b756c7ebd3da972
Mauricio Arismendi <arismendimauricio@gmail.com>
2019-09-25 23:42:10 -0300

    register component and navigation toolbar added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <mat-toolbar>
+      {{title}}
+      <span style="flex: 1 1 auto"></span>
+      <button mat-button routerLink="/register"> Register </button>
+    </mat-toolbar>
+    <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MEAN STACK';
+  title = 'Starter Full Stack Web';
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MessageComponent } from './messages.component';
+import { RegisterComponent } from './register.component';
 import { ApiService } from './api.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MessageComponent
+    MessageComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +23,8 @@ import { MatCardModule } from '@angular/material/card';
     HttpClientModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
